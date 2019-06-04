f35e76376c199205868b549aa6f5407944defdd4
elleremo <elleremo@yandex.ru>
2019-06-04 15:49:08 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+console.log('hi');
+var Day;
+(function (Day) {
+    Day["mon"] = "\u043F\u043E\u043D\u0435\u0434\u0435\u043B\u044C\u043D\u0438\u043A";
+    Day["tue"] = "\u0432\u0442\u043E\u0440\u043D\u0438\u043A";
+})(Day || (Day = {}));
+;
+let day = Day.mon;
+//# sourceMappingURL=main.js.map
---
+++ ts/main.ts
@@ -0,0 +1,8 @@
+console.log('hi');
+
+enum Day {
+    mon = 'понедельник', tue = 'вторник'
+};
+
+let day: Day = Day.mon;
+
